37f3f7b77eb1ba50df27b402cc3fe1c32e72264d
Your Name <prathamchaudharyyy@gmail.com>
2024-02-20 15:41:14 +0530

    Project 07

--- 07ReactRouter/src/main.jsx
+++ 07ReactRouter/src/main.jsx
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import User from './components/User.jsx'
+import Github, { githubInfoLoader } from './components/Github.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element = {<App />}>
+      <Route path='' element ={<Home/>}/>
+      <Route path='about' element ={<About/>}/>
+      <Route path='contact' element ={<Contact/>}/>
+      <Route path='user/:userid' element ={<User/>}/>
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element ={<Github/>}/>
       
     </Route>
   )
