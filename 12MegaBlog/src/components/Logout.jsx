4ca18e0c0f9c3bfd4be725a3cb48db09466f29db
Your Name <prathamchaudharyyy@gmail.com>
2024-03-05 15:04:54 +0530

    Blog Project

---
+++ 12MegaBlog/src/components/Logout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { UseDispatch } from "react-redux";
+import authService from "../appwrite/auth";
+
+function Logout(){
+    return(
+        <div></div>
+    )
+}
+
+export default Logout
