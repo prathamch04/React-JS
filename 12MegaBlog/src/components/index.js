a407d72ab54d3f6d099975e3ae5ca6da15839a30
Your Name <prathamchaudharyyy@gmail.com>
2024-03-07 23:49:15 +0530

    Blog Project

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer";
 import Container from "./Container";
 import Logo from "./Logo";
 import Logout from "./Logout";
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    Logout
+    Logout,
+    RTE
 }
